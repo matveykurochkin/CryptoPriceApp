878b71b57a6889b6ab3f695a570edfeb5e860867
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-03-31 12:57:12 +0300

    add method for parse reference

---
+++ CryptoPriceApp/CryptoPriceApp/Models/CryptoPriceInformation.cs
@@ -0,0 +1,37 @@
+﻿namespace CryptoPriceApp.Models
+{
+    public class CryptoPriceInformation
+    {
+        public int bitcoinPrice;
+        public double cardanoPrice, dogecoinPrice, ethereumPrice, litecoinPrice, tetherPrice;
+        public double bitcoinChange;
+        public async Task CryptoPrice()
+        {
+            string url = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                            .Build()
+                                            .GetSection("CryptoPriceURL")["cryptoPrice"];
+
+            HttpClient client = new HttpClient();
+
+            Root? cryptoPrice = await client.GetFromJsonAsync<Root>(url);
+
+            if (cryptoPrice is not null)
+            {
+                bitcoinPrice = cryptoPrice.bitcoin!.usd;
+                bitcoinChange = cryptoPrice.bitcoin!.usd_24h_change;
+
+                cardanoPrice = cryptoPrice.cardano!.usd;
+
+                dogecoinPrice = cryptoPrice.dogecoin!.usd;
+
+                ethereumPrice = cryptoPrice.ethereum!.usd;
+
+                litecoinPrice = cryptoPrice.litecoin!.usd;
+
+                tetherPrice = cryptoPrice.tether!.usd;
+            }
+
+        }
+
+    }
+}
