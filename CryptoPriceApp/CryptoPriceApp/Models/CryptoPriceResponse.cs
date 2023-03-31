9cfd6bfea28b1444c7f7092df54904e50e619fd5
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-03-31 12:58:15 +0300

    add class with data for response API crypto price

---
+++ CryptoPriceApp/CryptoPriceApp/Models/CryptoPriceResponse.cs
@@ -0,0 +1,60 @@
+﻿namespace CryptoPriceApp.Models
+{
+    public class Bitcoin
+    {
+        public int usd { get; set; }
+        public double usd_market_cap { get; set; }
+        public double usd_24h_vol { get; set; }
+        public double usd_24h_change { get; set; }
+        public int last_updated_at { get; set; }
+    }
+    public class Cardano
+    {
+        public double usd { get; set; }
+        public double usd_market_cap { get; set; }
+        public double usd_24h_vol { get; set; }
+        public double usd_24h_change { get; set; }
+        public int last_updated_at { get; set; }
+    }
+    public class Dogecoin
+    {
+        public double usd { get; set; }
+        public double usd_market_cap { get; set; }
+        public double usd_24h_vol { get; set; }
+        public double usd_24h_change { get; set; }
+        public int last_updated_at { get; set; }
+    }
+    public class Ethereum
+    {
+        public double usd { get; set; }
+        public double usd_market_cap { get; set; }
+        public double usd_24h_vol { get; set; }
+        public double usd_24h_change { get; set; }
+        public int last_updated_at { get; set; }
+    }
+    public class Litecoin
+    {
+        public double usd { get; set; }
+        public double usd_market_cap { get; set; }
+        public double usd_24h_vol { get; set; }
+        public double usd_24h_change { get; set; }
+        public int last_updated_at { get; set; }
+    }
+    public class Root
+    {
+        public Bitcoin? bitcoin { get; set; }
+        public Cardano? cardano { get; set; }
+        public Dogecoin? dogecoin { get; set; }
+        public Ethereum? ethereum { get; set; }
+        public Litecoin? litecoin { get; set; }
+        public Tether? tether { get; set; }
+    }
+    public class Tether
+    {
+        public double usd { get; set; }
+        public double usd_market_cap { get; set; }
+        public double usd_24h_vol { get; set; }
+        public double usd_24h_change { get; set; }
+        public int last_updated_at { get; set; }
+    }
+}
