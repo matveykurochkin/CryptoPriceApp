3d85be1653abf6bf2fa05d66981a34927b405059
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-04-01 22:14:04 +0300

    update logs

--- CryptoPriceApp/CryptoPriceApp/Controllers/HomeController.cs
+++ CryptoPriceApp/CryptoPriceApp/Controllers/HomeController.cs
@@ -15,17 +15,39 @@ namespace CryptoPriceApp.Controllers
 
         public async Task<IActionResult> Index()
         {
+            CryptoPriceInformation cryptoPriceInformation = new CryptoPriceInformation();
             try
             {
-                CryptoPriceInformation cryptoPriceInformation = new CryptoPriceInformation();
                 await cryptoPriceInformation.CryptoPrice();
-                _logger.LogInformation($"Success Response!");
+                _logger.LogInformation($"Success Response!\n" +
+                    $"\tBitcoin:\n" +
+                    $"\t\tPrise: {cryptoPriceInformation.bitcoinPrice} $\n" +
+                    $"\t\tChange: {cryptoPriceInformation.bitcoinChange} %\n" +
+                    $"\tEthereum:\n" +
+                    $"\t\tPrise: {cryptoPriceInformation.ethereumPrice} $\n" +
+                    $"\t\tChange: {cryptoPriceInformation.ethereumChange} %\n" +
+                    $"\tCardano:\n" +
+                    $"\t\tPrise: {cryptoPriceInformation.cardanoPrice} $\n" +
+                    $"\t\tChange: {cryptoPriceInformation.cardanoChange} %\n" +
+                    $"\tDogecoin:\n" +
+                    $"\t\tPrise: {cryptoPriceInformation.dogecoinPrice} $\n" +
+                    $"\t\tChange: {cryptoPriceInformation.dogecoinChange} %\n" +
+                    $"\tLitecoin:\n" +
+                    $"\t\tPrise: {cryptoPriceInformation.litecoinPrice} $\n" +
+                    $"\t\tChange: {cryptoPriceInformation.litecoinChange} %\n" +
+                    $"\tTether:\n" +
+                    $"\t\tPrise: {cryptoPriceInformation.tetherPrice} $\n" +
+                    $"\t\tChange: {cryptoPriceInformation.tetherChange} %\n");
                 return View("Index", cryptoPriceInformation);
             }
             catch (Exception ex)
             {
                 _logger.LogInformation($"Error message: {ex.Message}");
                 return View("Index");
+            }
+            finally
+            {
+                _logger.LogInformation($"Your URL: {cryptoPriceInformation.url}");
             }
         }
 
